fecf4e15f31a01ea370308430b934dfd8d3f6a43
Hyman <1604723751@qq.com>
2015-11-16 18:38:01 +0800

    Add some samples about thread feature to the solution.

---
+++ DNETBasic/ThreadBasic/BasicInvokeByDelegate/Client.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadBasic.BasicInvoke
+{
+    public delegate int TakesAWhileDelegate(int data, int ms);
+    public class Client
+    {
+        public static void Main1(string[] args)
+        {
+            TakesAWhileDelegate de = TakesAWhile;
+            
+            ////We will invoke the delgate asynchronous
+            //IAsyncResult ar = de.BeginInvoke(1, 30000, null, null);
+            //while(!ar.IsCompleted)
+            //{
+            //    Console.Write(".");
+            //    Thread.Sleep(50);
+            //}
+
+            //int result = de.EndInvoke(ar);
+            //Console.WriteLine("Result: {0}", result);
+            de.BeginInvoke(1, 3000, TakesAWhileCompleted, de);
+            for (int i = 0; i < 100; i++ )
+            {
+                Console.WriteLine(".");
+                Thread.Sleep(50);
+
+            }
+                Console.ReadKey();
+
+        }
+
+        public static int TakesAWhile(int data, int ms)
+        {
+            Console.WriteLine("TakesAWhile started");
+            Thread.Sleep(ms);
+            Console.WriteLine("TakesAWhile completed");
+            return ++data;
+        }
+
+        public static void TakesAWhileCompleted(IAsyncResult ar)
+        {
+            if(ar == null)
+            {
+                throw new ArgumentNullException("AR");
+            }
+
+            TakesAWhileDelegate dl = ar.AsyncState as TakesAWhileDelegate;
+            Trace.Assert(dl != null, "Invalid object type");
+
+            int result = dl.EndInvoke(ar);
+            Console.WriteLine("Result:{0}", result);
+        }
+    }
+}
---
+++ DNETBasic/ThreadBasic/BasicInvokeByThread/Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadBasic.BasicInvokeByThread
+{
+    public struct Data
+    {
+        public string Message;
+    }
+    public class Client
+    {
+        public static void Main(string[] args)
+        {
+            //var t = new Thread(ThreadMain);
+            //var t = new Thread(() => Console.WriteLine("Another thread"));
+            var data = new Data() { Message = "Hello" };
+            var t = new Thread(ThreadMain);
+            t.Start(data);
+
+            Console.WriteLine("This is the main thread");
+            Console.ReadKey();
+        }
+
+        public static void ThreadMain(object o)
+        {
+            Data d = (Data)o;
+            Console.WriteLine("Received Message:{0}", d.Message);
+            Console.WriteLine("Running in a thread");
+        }
+    }
+}
