e70d4cc3487d83b2f2920488d877ff0028734ce4
Hyman <1604723751@qq.com>
2015-11-19 20:48:43 +0800

    Add some note about thread to the solution

--- DNETBasic/ThreadBasic/BasicInvokeByThread/Client.cs
+++ DNETBasic/ThreadBasic/BasicInvokeByThread/Client.cs
@@ -18,17 +18,18 @@ namespace ThreadBasic.BasicInvokeByThread
             //var t = new Thread(ThreadMain);
             //var t = new Thread(() => Console.WriteLine("Another thread"));
             var data = new Data() { Message = "Hello" };
-            var t = new Thread(ThreadMain);
+            var t = new Thread(ThreadMain) { Name="Test", IsBackground = true};
             t.Start(data);
 
             Console.WriteLine("This is the main thread");
-            Console.ReadKey();
+            //Console.ReadKey();
         }
 
         public static void ThreadMain(object o)
         {
             Data d = (Data)o;
             Console.WriteLine("Received Message:{0}", d.Message);
+            Thread.Sleep(3000);
             Console.WriteLine("Running in a thread");
         }
     }
