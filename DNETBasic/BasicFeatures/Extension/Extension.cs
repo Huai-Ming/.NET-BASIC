21ac8a478b766430990334f37e8fca10dc295cb1
chenjiasheng <jacky_cjs@163.com>
2016-07-27 09:46:59 +0800

    Add Extension method test

---
+++ DNETBasic/BasicFeatures/Extension/Extension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFeatures.Extension
+{
+    class Extension
+    {
+        private static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+
+            Console.WriteLine(date.GetNearFirstDate().FormattedDateString());
+            Console.Read();
+        }
+    }
+
+    public static class TypeExtension
+    {
+        public static DateTime GetNearFirstDate(this DateTime date)
+        {
+            if (date.Day == 1)
+            {
+                return date;
+            }
+
+            DateTime nextDate = date.AddMonths(1);
+
+            return new DateTime(nextDate.Year, nextDate.Month, 1);
+        }
+
+        public static string FormattedDateString(this DateTime date)
+        {
+            return date.ToString("yyyy-MM-dd");
+        }
+    }
+}
