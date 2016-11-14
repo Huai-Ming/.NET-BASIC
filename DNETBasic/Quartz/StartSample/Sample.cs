a936100e9261dfb7b19d1f1fdf484000cc6d716a
Hyman <1604723751@qq.com>
2016-11-14 15:11:48 +0800

    Add AutoResetEvent and ManualResetEvent samples.

---
+++ DNETBasic/Quartz/StartSample/Sample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Quartz.Impl;
+
+namespace Quartz.StartSample
+{
+    class Sample
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Grab the Scheduler instance from the Factory 
+                IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+
+                // and start it off
+                scheduler.Start();
+
+                // some sleep to show what's happening
+                Thread.Sleep(TimeSpan.FromSeconds(60));
+
+                // and last shut down the scheduler when you are ready to close your program
+                scheduler.Shutdown();
+            }
+            catch (SchedulerException se)
+            {
+                Console.WriteLine(se);
+            }
+
+            Console.Read();
+        }
+    }
+}
